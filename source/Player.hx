2ef809015c68ff305640e42284ede63ddc56e3e1
Luramoth <fasreaperearnest@gmail.com>
2022-12-09 20:16:24 -0800

    add graphics and animations to the player

--- source/Player.hx
+++ source/Player.hx
@@ -12,13 +12,34 @@ class Player extends FlxSprite
 	{
 		super(x,y);
 
-		makeGraphic(16,16,FlxColor.BLUE);
+		// load player sprite graphics
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+
+		// give movement some drag
 		drag.x = drag.y = 800;
+
+		//make it so the sprite will flip around based on what direction it faces
+		setFacingFlip(LEFT, false, false);
+		setFacingFlip(RIGHT,true, false);
+
+		// make the hitbox match the graphics
+		setSize(8,8);
+		offset.set(4,8);
+
+		//add in some animations
+		animation.add("d_idle", [0]);
+		animation.add("lr_idle", [3]);
+		animation.add("u_idle", [6]);
+		animation.add("d_walk", [0,1,0,2], 6);
+		animation.add("lr_walk", [3,4,3,5], 6);
+		animation.add("u_walk", [6,7,6,8], 6);
 	}
 
 	//this function checks for if movement is being done
 	function updateMovement ()
 	{
+		var action:String = "idle";
+
 		var up:Bool = false;
 		var down:Bool = false;
 		var left:Bool = false;
@@ -49,6 +70,7 @@ class Player extends FlxSprite
 					newAngle -= 45;
 				else if (right)
 					newAngle += 45;
+				facing = UP;
 			}
 			else if (down)
 			{
@@ -57,14 +79,37 @@ class Player extends FlxSprite
 					newAngle += 45;
 				else if (right)
 					newAngle -= 45;
+				facing = DOWN;
 			}
 			else if (left)
-				newAngle = 180;
+				{
+					newAngle = 180;
+					facing = LEFT;
+				}
 			else if (right)
-				newAngle = 0;
+				{
+					newAngle = 0;
+					facing = RIGHT;
+				}
 
 			//now set velocity based on our genorated angle
 			velocity.setPolarDegrees(SPEED, newAngle);
+		}
+
+		if ((velocity.x != 0 || velocity.y != 0) && touching == NONE)
+		{
+			action = "walk";
+		}
+
+		switch (facing)
+		{
+			case LEFT, RIGHT:
+				animation.play("lr_" + action);
+			case UP:
+				animation.play("u_" + action);
+			case DOWN:
+				animation.play("d_" + action);
+			case _:
 		}
 	}
 
