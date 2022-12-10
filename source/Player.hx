35865f1fb86971e4d2340778fb1d512db77f948a
Luramoth <fasreaperearnest@gmail.com>
2022-12-09 18:04:21 -0800

    create player class

---
+++ source/Player.hx
@@ -0,0 +1,12 @@
+package;
+
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite{
+    public function new(x:Float =0, y:Float = 0){
+        super(x,y);
+
+        makeGraphic(16,16,FlxColor.BLUE);
+    }
+}
